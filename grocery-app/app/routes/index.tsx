65abcded85dfe0dbe9e3af9f9c97d59d334b1faa
NullDivision <penumbra.scilicet@gmail.com>
2022-11-28 18:56:22 +0200

    feat: add some logic

--- grocery-app/app/routes/index.tsx
+++ grocery-app/app/routes/index.tsx
@@ -13,16 +13,25 @@ interface ActionResponse {
   values?: Record<string, string>;
 }
 
-export const action: ActionFunction = async ({ request }): Promise<TypedResponse<ActionResponse | never>> => {
-  const formData = await request.formData();
+const handleAdd = async (formData: FormData): Promise<TypedResponse<ActionResponse | never>> => {
   const name = formData.get('name');
+  const expiryDate = formData.get('expiryDate');
+  const errors: [string, string][] = [];
 
   if (typeof name !== 'string' || name.length === 0) {
-    return json({ errors: { name: 'Missing or invalid name' }, values: Object.fromEntries(formData) });
+    errors.push(['name', 'Missing or invalid name']);
+  }
+
+  if (typeof expiryDate !== 'string' || expiryDate.length === 0) {
+    errors.push(['expiryDate', 'Missing or invalid expiry date']);
+  }
+
+  if (errors.length > 0) {
+    return json({ errors: Object.fromEntries(errors), values: Object.fromEntries(formData) });
   }
 
   try {
-    await db.product.create({ data: { name } });
+    await db.product.create({ data: { expiryDate: new Date(expiryDate as string), name: `${name}` } });
 
     return redirect('/');
   } catch (error) {
@@ -30,7 +39,34 @@ export const action: ActionFunction = async ({ request }): Promise<TypedResponse
       return json({ errors: { name: 'Name already exists', values: Object.fromEntries(formData) } });
     }
 
+    console.error(error);
+
     return json({ errors: { _: 'An unknown error occured', values: Object.fromEntries(formData) } }, { status: 500 });
+  }
+}
+
+const handleDelete = async (formData: FormData): Promise<TypedResponse<never>> => {
+  const rowId = formData.get('row-id');
+
+  if (typeof rowId !== 'string' || rowId.length === 0) {
+    return null as never;
+  }
+
+  await db.product.delete({ where: { id: Number(rowId) } });
+
+  return redirect('/');
+};
+
+export const action: ActionFunction = async ({ request }): Promise<TypedResponse<ActionResponse | never>> => {
+  const formData = await request.formData();
+
+  switch (formData.get('formName')) {
+    case 'add':
+      return handleAdd(formData);
+    case 'delete':
+      return handleDelete(formData);
+    default:
+      throw new Error('Invalid form name');
   }
 };
 
@@ -49,22 +85,49 @@ export default function Index() {
         <Link to={{ search: 'request_add=true' }}>Add Item</Link>
       </header>
       <main>
-        <ul id="product-list">
-          {products.map(({ name }) => <li key={name}>{name}</li>)}
-        </ul>
+        <Form method="delete" reloadDocument>
+          <input type="hidden" name="formName" value="delete" />
+
+          <ul id="product-list">
+            {products.map(({ expiryDate, id, name }) => (
+              <li key={id}>
+                <h2>{name}</h2> [{new Date(expiryDate).toLocaleDateString()}]
+                <button name="row-id" value={id.toString()} type="submit">
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </Form>
       </main>
+
       {searchParams.get('request_add') && (
         <div className="modal">
-          <Form method="post">
+          <Form method="post" reloadDocument>
             <h1>Add a product</h1>
+
             <p>
               <label htmlFor="name">Name</label>
-              <input defaultValue={actionData?.values?.name} id="name" name="name" required type="text" />
+              <input
+                defaultValue={actionData?.values?.name}
+                id="name"
+                name="name"
+                required
+                type="text"
+              />
             </p>
             {actionData?.errors?.name && <p>{actionData.errors.name}</p>}
+
+            <p>
+              <label htmlFor="expiryDate">Expires on</label>
+              <input id="expiryDate" name="expiryDate" required type="date" />
+            </p>
+            {actionData?.errors?.expiryDate && <p>{actionData.errors.expiryDate}</p>}
+
             {actionData?.errors?._ && <p>{actionData.errors._}</p>}
+
             <Link to={{ search: '' }}>Cancel</Link>
-            <button type='submit'>Add</button>
+            <button name="formName" type="submit" value="add">Add</button>
           </Form>
         </div>
       )}
