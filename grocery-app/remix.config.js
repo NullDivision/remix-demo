67d58c58230b2a1dbd30f05cfb8b80dec389d2e9
NullDivision <penumbra.scilicet@gmail.com>
2022-11-29 17:44:43 +0200

    feat: add vercel server file

--- grocery-app/remix.config.js
+++ grocery-app/remix.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildTarget: "vercel",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
